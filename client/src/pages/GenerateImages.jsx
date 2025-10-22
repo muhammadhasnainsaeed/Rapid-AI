2bdeb227c3abd592e51502eaeca46fc4dd9f80b7
Hasnain saeed <ihasnain4@gmail.com>
2025-10-23 00:20:53 +0500

    create community, generate image, remove background, remove object, review resume pages

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,5 +1,91 @@
-import React from "react";
+import { Image, Sparkles } from "lucide-react";
+import { useState } from "react";
 
 export default function GenerateImages() {
-  return <div>GenerateImages</div>;
+  const imageStyles = [
+    "Realistic",
+    "Ghibli style",
+    "Anime style",
+    "Cartoon style",
+    "Fantasy style",
+    "Realistic style",
+    "3D style",
+    "Portrait style",
+  ];
+  const [selectedStyles, setSelectedStyles] = useState(imageStyles[0]);
+  const [input, setInput] = useState("");
+  const [publish, setPublish] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <textarea
+          rows="4"
+          onInput={(e) => setInput(e.target.value)}
+          value={input}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Describe what you want to see in the image.."
+          required
+        ></textarea>
+        <p className="mt-4 text-sm font-medium">Style</p>
+        <div className="mt-3 grid grid-cols-2 sm:grid-cols-3 gap-3 flex-wrap md:max-w-9/11">
+          {imageStyles.map((item, index) => (
+            <span
+              key={index}
+              onClick={() => setSelectedStyles(item)}
+              className={`text-xs text-center px-4 py-1 border rounded-full cursor-pointer ${
+                selectedStyles === item
+                  ? "bg-green-50 text-green-700"
+                  : "text-gray-500 border-gray-300"
+              } `}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <div class="my-6 flex items-center gap-2">
+          <label class="relative cursor-pointer">
+            <input
+              class="sr-only peer"
+              onChange={(e) => setPublish(e.target.checked)}
+              checked={publish}
+              type="checkbox"
+            />
+            <div class="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition"></div>
+            <span class="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4"></span>
+          </label>
+          <p class="text-sm">Make this image Public</p>
+        </div>
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Image className="w-5" />
+          Generated image
+        </button>
+      </form>
+      {/* Right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <Image className="w-5 h-5 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">Generated image</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Image className="w-9 h-9" />
+            <p>Enter a topic and click “Generate image” to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/RemoveObject.jsx
+++ client/src/pages/RemoveObject.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import { Scissors, Sparkles } from "lucide-react";
+import { useState } from "react";
 
 export default function RemoveObject() {
-  return <div>RemoveObject</div>;
+  const [input, setInput] = useState("");
+  const [object, setObject] = useState("");
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Object Removal</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload image</p>
+        <input
+          type="file"
+          accept="image/*"
+          onInput={(e) => setInput(e.target.files[0])}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          required
+        />
+        <p class="text-xs text-gray-500 font-light mt-1">
+          Supports JPG, PNG, and other image formats
+        </p>
+        <p className="mt-6 text-sm font-medium">
+          Describe object name to remove
+        </p>
+        <textarea
+          rows="4"
+          onInput={(e) => setObject(e.target.value)}
+          value={object}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="e.g., watch or spoon , Only single object name"
+          required
+        ></textarea>
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Scissors className="w-5" />
+          Remove object
+        </button>
+      </form>
+      {/* Right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Scissors className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Processed Image</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Scissors className="w-9 h-9" />
+            <p>Upload an image and click "Remove Object" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
