63148af9072b23eb675589989fac812f961f4af5
m-zeeshan-saeed <63339889+muhammadhasnainsaeed@users.noreply.github.com>
2025-10-24 02:17:44 +0500

    create generate blog, generate image api

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import sql from "../config/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
 
 const openai = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -47,6 +49,96 @@ export const generateArticle = async (req, res) => {
       });
     }
     res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.json({
+        success: true,
+        message: "Limit reached, Upgrade to continue.",
+      });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_completion_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await sql` INSERT INTO creation (user_id, prompt, content, type)
+    VALUES (${userId}, ${prompt}, ${content}, 'blog-title')
+    `;
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+    res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: true,
+        message: "This feature is only available for premium subscription",
+      });
+    }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      FormData,
+      {
+        headers: {
+          "x-api-key": process.env.CLIP_DROP_API_KEY,
+        },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      data,
+      "binary"
+    ).toString("base64")}`;
+
+    const { secure_url } = await cloudinary.uploader.upload(base64Image);
+
+    await sql` INSERT INTO creation (user_id, prompt, content, type, publish)
+    VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})
+    `;
+
+    res.json({ success: true, content: secure_url });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { generateArticle } from "../controllers/aiController.js";
+import {
+  generateArticle,
+  generateBlogTitle,
+  generateImage,
+} from "../controllers/aiController.js";
 
 const aiRoutes = express.Router();
 
 aiRoutes.post("/generate-article", auth, generateArticle);
+aiRoutes.post("/generate-blog-title", auth, generateBlogTitle);
+aiRoutes.post("/generate-image", auth, generateImage);
 
 export default aiRoutes;
