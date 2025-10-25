78e379494b983cefbcd9ea67495d32647c6ae4ea
m-zeeshan-saeed <63339889+muhammadhasnainsaeed@users.noreply.github.com>
2025-10-25 20:12:28 +0500

    add user controllers files

---
+++ server/controllers/userController.js
@@ -0,0 +1,54 @@
+import sql from "../config/db.js";
+
+export const getUserCreations = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+
+    const creations = sql`SELECT * FROM creation WHERE user_id = ${userId} ORDER BY created_at DESC`;
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getPublishCreations = async (req, res) => {
+  try {
+    const creations = sql`SELECT * FROM creation WHERE publish = true ORDER BY created_at DESC`;
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const toggleLikesCreation = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+    const [creation] = await sql`SELECT * FROM creation WHERE id = ${id}`;
+
+    if (!creation) {
+      return res.json({ success: false, message: "Creation not found" });
+    }
+
+    const currentLikes = creation.likes;
+    const userIdStr = userId.toString();
+    let updatedLikes;
+    let message;
+
+    if (currentLikes.includes(userIdStr)) {
+      updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+      message = "Creation Disliked";
+    } else {
+      updatedLikes = [...currentLikes, userIdStr];
+      message = "Creation Liked";
+    }
+
+    const formattedArray = `{${updatedLikes.join(",")}}`;
+
+    await sql`UPDATE creation SET likes = ${formattedArray}::text[] WHERE id = ${id}`;
+
+    res.json({ success: true, message });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
